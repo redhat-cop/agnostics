fd16fcdf281db0317d180964a9d449d080e11dbd
Guillaume Coré <gucore@redhat.com>
2020-11-03 11:37:21 +0100

    Add authentication for the API (htpasswd) (#23)
    * Add authentication for the API (htpasswd)
    
    This commit, if applied, adds Basic HTTP authentication for the API
    using htpasswd.
    
    New dependency: github.com/tg123/go-htpasswd
    
    The authentication is enabled by default but can be disabled.
    
    All routes are behind authentication, except /health.
    
    * Add and mount secret for htpasswd
    
    * Address JK's comment, fix indentation

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-git/go-git/v5 v5.1.0
 	github.com/gomodule/redigo v1.8.2
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/tg123/go-htpasswd v1.0.0
 	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9 // indirect
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
 	golang.org/x/sys v0.0.0-20200610111108-226ff32320da // indirect
--- internal/api/api.go
+++ internal/api/api.go
@@ -6,6 +6,11 @@ import (
 	"github.com/redhat-gpe/agnostics/internal/db"
 	"github.com/redhat-gpe/agnostics/internal/log"
 	"io"
+	"strings"
+	"encoding/base64"
+	"bytes"
+	"github.com/tg123/go-htpasswd"
+	"path/filepath"
 )
 
 func healthHandler (w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
@@ -20,7 +25,39 @@ func healthHandler (w http.ResponseWriter, req *http.Request, _ httprouter.Param
 	io.WriteString(w, "OK\n")
 }
 
-func Serve(addr string) {
+func BasicAuth(h httprouter.Handle, myauth *htpasswd.File, authEnabled bool) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+		if ! authEnabled {
+			// Delegate request to the given handle
+			h(w, r, ps)
+			return
+		}
+
+		const basicAuthPrefix string = "Basic "
+
+		// Get the Basic Authentication credentials
+		auth := r.Header.Get("Authorization")
+		if strings.HasPrefix(auth, basicAuthPrefix) {
+			// Check credentials
+			payload, err := base64.StdEncoding.DecodeString(auth[len(basicAuthPrefix):])
+			if err == nil {
+				pair := bytes.SplitN(payload, []byte(":"), 2)
+				if len(pair) == 2 && myauth.Match(string(pair[0]), string(pair[1])) {
+					// Delegate request to the given handle
+					h(w, r, ps)
+					return
+				}
+			}
+		}
+
+		// Request Basic Authentication otherwise
+		w.Header().Set("WWW-Authenticate", "Basic realm=Restricted")
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+	}
+}
+
+func Serve(addr string, apiAuth bool, apiHtpasswd string) {
 	router := httprouter.New()
 
 	// Health and status checks
@@ -29,20 +66,40 @@ func Serve(addr string) {
 	router.GET("/api/v1/health", healthHandler)
 	router.GET("/api/v1/healthz", healthHandler)
 
+	// htpasswd authentication
+	if ! apiAuth {
+		apiHtpasswd = "/dev/null"
+		log.Out.Println("API authentication disabled")
+	}
+	absAPIHtpasswdPath, err := filepath.Abs(apiHtpasswd)
+	if err != nil {
+		log.Err.Println("ERROR with api-htpasswd-path")
+		log.Err.Fatal(err)
+	}
+	myauth, err := htpasswd.New(absAPIHtpasswdPath, htpasswd.DefaultSystems, nil)
+	if err != nil {
+		log.Err.Println("ERROR loading htpasswd", absAPIHtpasswdPath)
+		log.Err.Fatal(err)
+	} else {
+		if apiAuth {
+			log.Out.Println("htpasswd found:", absAPIHtpasswdPath)
+		}
+	}
+
 	// v1
-	router.GET("/api/v1/clouds", v1GetClouds)
-	router.GET("/api/v1/clouds/:name", v1GetCloudByName)
-	router.POST("/api/v1/taint/:cloudname", v1PostTaintByCloudName)
-	router.POST("/api/v1/taint/:cloudname/delete", v1DeleteTaintByCloudName)
-	router.DELETE("/api/v1/taint/:cloudname/:taintindex", v1DeleteTaintByIndex)
-	router.DELETE("/api/v1/taints/:cloudname", v1DeleteTaintsByCloudName)
-	router.GET("/api/v1/repo", v1GetRepository)
-	router.PUT("/api/v1/repo", v1PullRepository)
-	router.POST("/api/v1/schedule", v1PostSchedule)
-	router.GET("/api/v1/placements", v1GetPlacements)
-	router.GET("/api/v1/placements/:uuid", v1GetPlacement)
-	router.DELETE("/api/v1/placements/:uuid", v1DeletePlacement)
-	router.PUT("/api/v1/counters", v1PutCounters)
+	router.GET("/api/v1/clouds", BasicAuth(v1GetClouds, myauth, apiAuth))
+	router.GET("/api/v1/clouds/:name", BasicAuth(v1GetCloudByName, myauth, apiAuth))
+	router.POST("/api/v1/taint/:cloudname", BasicAuth(v1PostTaintByCloudName, myauth, apiAuth))
+	router.POST("/api/v1/taint/:cloudname/delete", BasicAuth(v1DeleteTaintByCloudName, myauth, apiAuth))
+	router.DELETE("/api/v1/taint/:cloudname/:taintindex", BasicAuth(v1DeleteTaintByIndex, myauth, apiAuth))
+	router.DELETE("/api/v1/taints/:cloudname", BasicAuth(v1DeleteTaintsByCloudName, myauth, apiAuth))
+	router.GET("/api/v1/repo", BasicAuth(v1GetRepository, myauth, apiAuth))
+	router.PUT("/api/v1/repo", BasicAuth(v1PullRepository, myauth, apiAuth))
+	router.POST("/api/v1/schedule", BasicAuth(v1PostSchedule, myauth, apiAuth))
+	router.GET("/api/v1/placements", BasicAuth(v1GetPlacements, myauth, apiAuth))
+	router.GET("/api/v1/placements/:uuid", BasicAuth(v1GetPlacement, myauth, apiAuth))
+	router.DELETE("/api/v1/placements/:uuid", BasicAuth(v1DeletePlacement, myauth, apiAuth))
+	router.PUT("/api/v1/counters", BasicAuth(v1PutCounters, myauth, apiAuth))
 
 	log.Out.Println("API listen on port", addr)
 	log.Err.Fatal(http.ListenAndServe(addr, router))
